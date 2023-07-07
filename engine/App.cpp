6a01e5d64d2fd8dd8b4c69e878c7580cb4afe3d8
TeriyakiGod <kacperochnik01@gmail.com>
2023-07-07 10:26:08 +0200

    Project Init

---
+++ engine/App.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "App.h"
+
+namespace koe {
+
+        App::App() {
+            std::cout << "Hello, World!" << std::endl;
+        }
+
+        App::~App() = default;
+
+        void App::run() {
+            
+        }
+}
---
+++ engine/App.h
@@ -0,0 +1,15 @@
+#ifndef KOE_APP_H
+#define KOE_APP_H
+
+namespace koe {
+
+    class App {
+    public:
+        App();
+        ~App();
+        void run();
+    };
+
+}
+
+#endif
